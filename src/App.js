6ad7ff082375926cfe6a1c081269fca2da16050f
KrishnaDass95 <krishdass95@gmail.com>
2023-09-07 11:15:10 +0530

    Clean up and create base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <form className="todo-form">
+      <label htmlFor="todo">Enter new item</label>
+      <input type="text" placeholder="write here" id="todo"></input>
+      <button>Add TODO</button>
+    </form>
+    <h1>Todos</h1>
+    <label>
+      <input type="checkbox"></input>
+      First todo
+    </label>
+    <button>Delete</button>
+    </>
+  )
 }
-
 export default App;
