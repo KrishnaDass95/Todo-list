2df1cb49f12d88589287ffa4acc43f77be34130b
KrishnaDass95 <krishdass95@gmail.com>
2023-09-07 11:41:52 +0530

    Add state to your project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import "./style.css"
+import { useState } from "react";
+import "./style.css";
 
 const App = () => {
+  const [newItem, setNewItem] = useState("");
+
   return (
     <>
-    <form className="todo-form">
-      <label htmlFor="todo">Enter new item</label>
-      <input type="text" placeholder="write here" id="todo"></input>
-      <button>Add TODO</button>
-    </form>
-    <h1>Todos</h1>
-    <label>
-      <input type="checkbox"></input>
-      First todo
-    </label>
-    <button>Delete</button>
+      <form className="todo-form">
+        <label htmlFor="todo">Enter new item</label>
+        <input
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          placeholder="write here"
+          id="todo"
+        ></input>
+        <button>Add TODO</button>
+      </form>
+      <h1>Todos</h1>
+      <label>
+        <input type="checkbox"></input>
+        First todo
+      </label>
+      <button>Delete</button>
     </>
-  )
-}
+  );
+};
 export default App;
