0287cc8c26a6128cb6d1ab130ee6b822bbadbd22
KrishnaDass95 <krishdass95@gmail.com>
2023-09-12 10:53:56 +0530

    Passing prop function from parent to child

--- src/App.js
+++ src/App.js
@@ -1,75 +1,63 @@
 import { useState } from "react";
 import "./style.css";
+import Form from "./components/Form";
 
 const App = () => {
-  // this is for the search field
-  const [newItem, setNewItem] = useState("");
   // array that holds all the todos in an object form
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (e) => {
-    // form does not refresh
-    e.preventDefault();
-    
-    // Whenever you need values from a previous state, you need to use a function to call prev state 
+  function addTodo(title){
+    // Whenever you need values from a previous state, you need to use a function to call prev state
     // and return a new state
     setTodos((currentState) => {
       return [
         ...currentState,
         {
           id: crypto.randomUUID(),
-          title: newItem,
+          title,
           completed: false,
         },
       ];
     });
-    setNewItem("");
-  };
+  }
+  
+  
 
-  function toggleTodo(id, completed){
-    setTodos(currentState => {
-      return currentState.map(todo => {
-        if(todo.id === id){
-          return {...todo, completed}
+  function toggleTodo(id, completed) {
+    setTodos((currentState) => {
+      return currentState.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
         }
 
-        return todo
-      })
-    })
+        return todo;
+      });
+    });
   }
 
   const deleteTodo = (id) => {
-    setTodos(currentState => {
-      return currentState.filter(todo => todo.id !== id)
-    })
-
-  }
+    setTodos((currentState) => {
+      return currentState.filter((todo) => todo.id !== id);
+    });
+  };
 
   return (
     <>
-      <form onSubmit={addTodo} className="todo-form">
-        <label htmlFor="todo">Enter new item</label>
-        <input
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-          type="text"
-          placeholder="write here"
-          id="todo"
-        ></input>
-        <button>Add TODO</button>
-      </form>
+    <Form addTodo={addTodo}/>
       <h1>Todos</h1>
       {todos.length === 0 && "No Todos"}
       {todos.map((todo) => {
         return (
           <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed}
-              onChange={e => toggleTodo(todo.id, e.target.completed)}
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => toggleTodo(todo.id, e.target.completed)}
               ></input>
               {todo.title}
             </label>
-            <button onClick={e => deleteTodo(todo.id)}>Delete</button>
+            <button onClick={(e) => deleteTodo(todo.id)}>Delete</button>
           </li>
         );
       })}
---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Form = ({ addTodo }) => {
+  // this is for the search field
+  const [newItem, setNewItem] = useState("");
+
+  const handleSubmit = (e) => {
+    // form does not refresh
+    e.preventDefault();
+
+    addTodo(newItem);
+
+    
+    setNewItem("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <label htmlFor="todo">Enter new item</label>
+      <input
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+        type="text"
+        placeholder="write here"
+        id="todo"
+      ></input>
+      <button>Add TODO</button>
+    </form>
+  );
+};
+
+export default Form;
